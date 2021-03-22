f3b5ae5e91bd2a9be330a0e67b84325e93e1a3ab
Piotr Slowik <piotrslowik409@gmail.com>
2021-03-22 13:40:09 +0100

    Signup Process TEST

--- track-server/src/Index.js
+++ track-server/src/Index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+app.use(authRoutes);
 
 const mongoUri= 'mongodb+srv://admin:password89@cluster0.czwoy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
